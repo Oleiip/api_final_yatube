d79d3562a4ce70734bbb188159ecb70a13bd732a
oleii <oleiip@yandex.com>
2022-11-04 10:43:05 +0300

    add generics

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Group, Post, User, Follow
-from rest_framework import permissions, viewsets, status, filters
-from rest_framework.response import Response
+from posts.models import Group, Post, User
+from rest_framework import permissions, viewsets, filters, mixins
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.pagination import LimitOffsetPagination
 from api.permissions import IsAuthorOrReadOnlyPermission
 from api.serializers import (
     CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer)
@@ -15,6 +15,7 @@ class PostViewSet(viewsets.ModelViewSet):
         IsAuthenticatedOrReadOnly,
         IsAuthorOrReadOnlyPermission
     ]
+    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -44,17 +45,17 @@ class CommentViewSet(viewsets.ModelViewSet):
         ).comments
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [IsAuthorOrReadOnlyPermission]
 
-    def create(self, request):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-
-class FollowViewSet(viewsets.ModelViewSet):
-    queryset = Follow.objects.all()
+class FollowViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
     serializer_class = FollowSerializer
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = [filters.SearchFilter]
